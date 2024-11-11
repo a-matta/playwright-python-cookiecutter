59103fe7e067df1a22ff39064f387f6772bd87dc
Amrita Matta <matta.amrita@gmail.com>
2024-11-11 20:31:06 +0200

    feat: add slowmo option
    Signed-off-by: Amrita Matta <matta.amrita@gmail.com>

--- {{cookiecutter.project_slug}}/tasks.py
+++ {{cookiecutter.project_slug}}/tasks.py
@@ -23,19 +23,23 @@ def clean(context):
 
 
 @task(pre=[clean])
-def tests(context, headed=False, browser="chromium", tracing=False, video=False):
+def tests(context, headed=False, browser="chromium", slowmo="0", tracing=False, video=False):
     """Run the tests in 'tests' directory.
 
     Args:
         headed: Run the tests in headed mode. Defaults to headless mode.
         browser: Browser to run the tests. Defaults to "chromium".
+        slowmo: Whether to run the tests in slow motion. Disabled by default.
         tracing: Whether to record a trace for failing test. Disabled by default.
         video: Whether to record a video for failing test. Disabled by default.
+
+    For reference: see https://playwright.dev/python/docs/test-runners#cli-arguments
     """
     cmd = [
         "pytest",
         "--headed" if headed else "",
         f"--browser {browser}",
+        f"--slowmo={slowmo}",
         "--tracing=retain-on-failure" if tracing else "",
         "--video=retain-on-failure" if video else "",
         f"{ROOT}/tests",
